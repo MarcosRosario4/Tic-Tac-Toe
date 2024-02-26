9dcfe5f37afd428443ff9abeafdda744ba7ea6dc
Macbook <macbook@Catherines-MBP.lan>
2024-02-26 18:48:10 -0500

    added board and function init to main.js

--- js/js/main.js
+++ js/js/main.js
@@ -1,5 +1,17 @@
 /*----- constants -----*/
 /*----- app's state (variables) -----*/
+let board;
+function init() {
+    board = [
+    '', '', '',
+    '', '', '',
+    '', '', ''
+    ];
+    };
+    //be sure to call the init function!
+    init();
 /*----- cached element references -----*/
 /*----- event listeners -----*/
 /*----- functions -----*/
+function render() {
+}
