d60c1c273575e3f884f4a31efe74f307cd3ea735
Macbook <macbook@Catherines-MBP.lan>
2024-02-26 20:02:53 -0500

    Updated main.js file with app state/functions

--- js/js/main.js
+++ js/js/main.js
@@ -1,17 +1,27 @@
 /*----- constants -----*/
 /*----- app's state (variables) -----*/
 let board;
+let turn = 'X';
+
+/*----- cached element references -----*/
+const squares = Array.from(document.querySelectorAll('#board div'));
+
+/*----- event listeners -----*/
+/*----- functions -----*/
 function init() {
     board = [
     '', '', '',
     '', '', '',
     '', '', ''
     ];
-    };
     //be sure to call the init function!
-    init();
-/*----- cached element references -----*/
-/*----- event listeners -----*/
-/*----- functions -----*/
+    render();
+};
+
 function render() {
-}
+    board.forEach(function(mark, index){
+        
+        //this sets the text content of the square of the same position to the mark on the board. 
+        squares[index].textContent = mark;
+        });
+};
